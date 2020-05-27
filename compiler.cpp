66cc288181df3c7a15aa885a03d8abebc9d0a298
Rob Hess <robwhess@gmail.com>
2020-05-27 12:05:04 -0700

    Add code from 5/27 lecture.

--- compiler.cpp
+++ compiler.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Module.h"
+
+static llvm::LLVMContext TheContext;
+static llvm::IRBuilder<> TheBuilder(TheContext);
+static llvm::Module* TheModule;
+
 int main(int argc, char const *argv[]) {
+  TheModule = new llvm::Module("LLVM_Demo", TheContext);
+
+  llvm::Function* fooFn = llvm::Function::Create();
+
+  delete TheModule;
   return 0;
 }
